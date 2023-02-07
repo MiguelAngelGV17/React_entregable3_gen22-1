01b748ea45ce4dfd7377386a8141bc329ddb23bc
MiguelAngelGV17 <mgonzalez94@hotmail.es>
2023-02-06 22:35:01 -0500

    Add suggestion functionality at the search inpt

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,16 @@ function App() {
 
   const [location, setLocation] = useState()
   const [numberLocation, setNumberLocation] = useState(getRandomLocation())
-  const [hasError, setHasError] = useState(false)
+  const [currentSelection, setCurrentSelection] = useState()
+  const [hasError, setHasError] = useState()
+  const [listLocation, setListLocation] = useState()
+  const [isShow, setIsShow] = useState(false)
+  const [selection, setSelection] = useState()
+  const [closeList, setCloseList] = useState(false)
 
 
   useEffect(() => {
-    const url = `https://rickandmortyapi.com/api/location/${numberLocation}`
-
+    const url = `https://rickandmortyapi.com/api/location/${numberLocation}` /*-- Buscar por numero de location*/
     axios.get(url)
       .then(res => {
         setLocation(res.data)
@@ -26,35 +30,76 @@ function App() {
         setHasError(true)
       }
       )
-
   }, [numberLocation])
 
-
+  const handleChange = e => {
+    const url = `https://rickandmortyapi.com/api/location/?name=${e.target.value.trim()}`
+    axios.get(url)
+    .then(res => setListLocation(res.data.results))
+    .catch(err => {
+      console.log(err)
+      setHasError(true)
+    })
+    setSelection(e.target.value.trim())
+  }
+  
   const handleSubmit = e => {
     e.preventDefault()
     if (e.target.inputLocation.value.trim().length === 0) {
       setNumberLocation(getRandomLocation)
-    }else{
-      setNumberLocation(e.target.inputLocation.value.trim())
+    } else {
+      setNumberLocation(currentSelection)
+      setIsShow(false)
     }
-    e.target.inputLocation.value = e.target.inputLocation.value.trim()
   }
-  console.log(location)
+
+  const handleFocus = e => setIsShow(true)
+  const handleBlur = () => {
+    setTimeout(() => {
+      setIsShow(false)
+    }, 120);
+  }
+
   return (
     <div className="app">
-      {/* <img className='app__bgImage' src="../banner.png" alt="" /> */}
       <div className='app__bgImage'></div>
-      {/* <h1 className='app__title'>Rick and Morty</h1> */}
       <form className='form' onSubmit={handleSubmit}>
-        <input className='form__input' id='inputLocation' type="text" placeholder='Type a number ID (1-126) '/>
+        <input
+          className='form__input'
+          id='inputLocation'
+          type="text"
+          value={selection}
+          onChange={handleChange}
+          onFocus={handleFocus}
+          onBlurCapture={handleBlur}
+        />
         <button className='form__btn'>Search</button>
       </form>
+      {
+        <ul className={`form__suggestion ${isShow}`}>
+          {
+            listLocation?.map(loc => (
+              <li
+                className='form__suggItem'
+                id='location'
+                onClick={e => {
+                  setCurrentSelection(loc.id)
+                  setSelection(loc.name)
+                }}
+                key={loc.id}
+              >
+                {loc.name}
+              </li>
+            ))
+          }
+        </ul>
+      }
       {
         hasError ?
           <h2 className='app__error'>❌ Hey! you must provide an id from 1 to 126 🥺</h2>
           :
           <>
-            <LocationInfo location={location}/>
+            <LocationInfo location={location} />
             <div className='residents__container'>
               {
                 location?.residents.map(url => (
